8c690821ef87037755e72c83a748af9a285cd3b5
Om Prakash <op.omprakash1921@gmail.com>
2026-07-07 11:27:19 +0530

    Done With About Section

--- Portfolio-master/src/components/About/AboutCard.js
+++ Portfolio-master/src/components/About/AboutCard.js
@@ -8,14 +8,12 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Om Prakash </span>
+            from <span className="purple"> Bihar, India.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently persuing Bachelor's Degree from LNCT Group of College.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
-            <br />
+            
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
@@ -34,7 +32,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">OmPrakash</footer>
         </blockquote>
       </Card.Body>
     </Card>
--- Portfolio-master/src/components/About/Techstack.js
+++ Portfolio-master/src/components/About/Techstack.js
@@ -28,18 +28,14 @@ function Techstack() {
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <TbBrandGolang />
-      </Col>
+      
       <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSolidity />
-      </Col>
+      
       <Col xs={4} md={2} className="tech-icons">
         <DiMongodb />
       </Col>
@@ -55,9 +51,7 @@ function Techstack() {
       <Col xs={4} md={2} className="tech-icons">
         <SiRedis />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostgresql />
-      </Col>
+      
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
       </Col>
